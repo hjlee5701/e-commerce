29f1152ecf36bfbbe92ee4026d617507aa72dccc
hjlee5701 <hjlee5701@gmail.com>
2025-04-09 01:35:11 +0900

    [FEAT] 테스트용 공통 Fixture 생성

---
+++ src/test/java/kr/hhplus/be/server/common/FixtureTestSupport.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.common;
+
+import kr.hhplus.be.server.domain.member.Member;
+
+
+public abstract class FixtureTestSupport {
+    public static final Long ANY_MEMBER_ID = 1L;
+    public static final Member ANY_MEMBER = new Member();
+
+}
