639c42dfe034a51fdc5d4b05164202c9f7c82f87
hjlee5701 <hjlee5701@gmail.com>
2025-04-11 04:37:59 +0900

    [TEST] OrderFactoryTest 단위 테스트

--- src/main/java/kr/hhplus/be/server/domain/order/OrderFactory.java
+++ src/main/java/kr/hhplus/be/server/domain/order/OrderFactory.java
@@ -1,22 +1,20 @@
 package kr.hhplus.be.server.domain.order;
 
-import java.time.LocalDateTime;
-
 public class OrderFactory {
 
     public static Order create(OrderCommand.Create command) {
         return new Order(
-                1L,
+                null,
                 command.getMember(),
                 command.getTotalAmount(),
                 OrderStatus.PENDING,
-                LocalDateTime.now()
+                null
         );
     }
 
     public static OrderItem createItem(Order order, OrderCommand.ItemCreate command) {
         return new OrderItem(
-                1L,
+                null,
                 command.getProduct().getTitle(),
                 order,
                 command.getProduct(),
---
+++ src/test/java/kr/hhplus/be/server/domain/order/OrderFixture.java
@@ -0,0 +1,33 @@
+package kr.hhplus.be.server.domain.order;
+
+import kr.hhplus.be.server.common.FixtureReflectionUtils;
+import kr.hhplus.be.server.common.TestFixture;
+import kr.hhplus.be.server.domain.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class OrderFixture implements TestFixture<Order> {
+
+    private final Long id = 1L;
+    private Member member = new Member();
+
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    private OrderStatus status = OrderStatus.PENDING;
+
+    private LocalDateTime orderedAt = LocalDateTime.now();
+
+    @Override
+    public Order create() {
+        Order entity = new Order();
+        FixtureReflectionUtils.reflect(entity, this);
+        return entity;
+    }
+}
