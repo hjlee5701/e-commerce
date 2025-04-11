9014a244a6e4264a2b92b5ac4500020ba3f630db
hjlee5701 <hjlee5701@gmail.com>
2025-04-11 09:16:23 +0900

    [FIX] ProductStockRepositoryImpl 의 @Component 누락

--- src/main/java/kr/hhplus/be/server/infrastructure/product/ProductStockRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/product/ProductStockRepositoryImpl.java
@@ -2,7 +2,9 @@ package kr.hhplus.be.server.infrastructure.product;
 
 import kr.hhplus.be.server.domain.product.ProductStock;
 import kr.hhplus.be.server.domain.product.ProductStockRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductStockRepositoryImpl implements ProductStockRepository {
     @Override
     public ProductStock save(ProductStock productStock) {
