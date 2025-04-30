8a609e486d80bfde58e858447f0a1994605c08a8
hjlee5701 <hjlee5701@gmail.com>
2025-05-01 00:01:16 +0900

    [CHORE] WebConfig 에 interceptor 의존성 주입

--- src/main/java/kr/hhplus/be/server/shared/config/WebConfig.java
+++ src/main/java/kr/hhplus/be/server/shared/config/WebConfig.java
@@ -1,16 +1,20 @@
 package kr.hhplus.be.server.shared.config;
 
 import kr.hhplus.be.server.shared.logging.RequestTimingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final RequestTimingInterceptor executionTimeInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestTimingInterceptor())
-                .addPathPatterns("/**"); // 모든 경로에 적용
+        registry.addInterceptor(executionTimeInterceptor)
+                .addPathPatterns("/**"); // 모든 요청에 적용
     }
 }
