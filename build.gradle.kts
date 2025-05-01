766e1d5f1795586d8177ac568ffddf5d26448101
hjlee5701 <hjlee5701@gmail.com>
2025-05-02 00:00:31 +0900

    [FEAT] Redis 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,10 @@ dependencies {
 	// instancio
 	implementation("org.instancio:instancio-core:5.4.0")
 
+	// Redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.redisson:redisson-spring-boot-starter:3.17.6")
+
 }
 
 tasks.withType<Test> {
