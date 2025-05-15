ec6ba772a8d6d3a76da1211e352ca5671e96c699
hjlee5701 <hjlee5701@gmail.com>
2025-05-16 07:27:01 +0900

    [feat] 쿠폰 발급 Redis 적용

---
+++ src/main/java/kr/hhplus/be/server/interfaces/coupon/CouponScheduler.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.interfaces.coupon;
+
+import kr.hhplus.be.server.domain.coupon.Coupon;
+import kr.hhplus.be.server.domain.coupon.CouponService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CouponScheduler {
+
+    private final CouponService couponService;
+
+    @Scheduled(cron = "0 * * * * *")
+    @Transactional
+    public void issueCoupon() {
+        List<Coupon> coupons = couponService.getAllAvailable();
+        for (Coupon coupon : coupons) {
+            couponService.processCoupon(coupon);
+        }
+    }
+}
